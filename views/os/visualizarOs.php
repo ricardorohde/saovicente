7440a45c5d8b21b175ed76e69374dd6b06b54dbf
Leandro <silveira.jedi@gmail.com>
2016-08-29 16:19:05 -0300

    Add files via upload

---
+++ views/os/visualizarOs.php
@@ -0,0 +1,206 @@
+<?php $totalServico = 0; $totalProdutos = 0;?>
+<div class="row-fluid" style="margin-top: 0">
+    <div class="span12">
+        <div class="widget-box">
+            <div class="widget-title">
+                <span class="icon">
+                    <i class="icon-tags"></i>
+                </span>
+                <h5>Ordem de Serviço</h5>
+                <div class="buttons">
+                    <?php if($this->permission->checkPermission($this->session->userdata('permissao'),'eOs')){
+                        echo '<a title="Icon Title" class="btn btn-mini btn-info" href="'.base_url().'index.php/os/editar/'.$result->idOs.'"><i class="icon-pencil icon-white"></i> Editar</a>'; 
+                    } ?>
+                    
+                    <a id="imprimir" title="Imprimir" class="btn btn-mini btn-inverse" href=""><i class="icon-print icon-white"></i> Imprimir</a>
+                </div>
+            </div>
+            <div class="widget-content" id="printOs">
+                <div class="invoice-content">
+                    <div class="invoice-head" style="margin-bottom: 0">
+
+                        <table class="table">
+                            <tbody>
+                                <?php if($emitente == null) {?>
+                                            
+                                <tr>
+                                    <td colspan="3" class="alert">Você precisa configurar os dados do emitente. >>><a href="<?php echo base_url(); ?>index.php/mapos/emitente">Configurar</a><<<</td>
+                                </tr>
+                                <?php } else {?>
+                                <tr>
+                                    <td style="width: 25%"><img src=" <?php echo $emitente[0]->url_logo; ?> "></td>
+                                    <td> <span style="font-size: 14px; "> <?php echo $emitente[0]->nome; ?></span> </br><span style="font-size: 10px; ">CNPJ: <?php echo $emitente[0]->cnpj; ?> </br> <?php echo $emitente[0]->rua.', nº:'.$emitente[0]->numero.', '.$emitente[0]->bairro.' - '.$emitente[0]->cidade.' - '.$emitente[0]->uf; ?> </span> </br> <span style="font-size: 10px; "> E-mail: <?php echo $emitente[0]->email.' - Fone: '.$emitente[0]->telefone; ?></span></td>
+                                    <td style="width: 18%; text-align: center"> #Protocolo: <span ><?php echo $result->idOs?></span></br> </br> <span>Emissão: <?php echo date('d/m/Y')?></span></td>
+                                </tr>
+
+                                <?php } ?>
+                            </tbody>
+                        </table>
+
+                
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td style="width: 50%; padding-left: 0">
+                                        <ul>
+                                            <li>
+                                                <span><h3>Cliente</h3></span>
+                                                <span style="font-size: 16px; "><?php echo $result->nomeCliente?></span><br/>
+                                                <span style="font-size: 16px; ">CPF: <?php echo $result->documento?><br/>
+                                                    <span style="font-size: 16px; "><?php echo $result->rua?>
+                                                        <span style="font-size: 16px;" >, <?php echo $result->numero?>
+                                                            <span style="font-size: 16px; "> - <?php echo $result->bairro?> Fone: <?php echo $result->telefone?>
+                                            </li>
+                                        </ul>
+                                    </td>
+                                    <td style="width: 25%; padding-left: 0"><span style="font-size: 10px; ">
+                                        <?php if($result->descricaoProduto != null){?>
+                    
+                                        <h3>KM / Placa</h3>
+                                        <p>
+                                         <span style="font-size: 16px; "> <?php echo $result->descricaoProduto?>
+                        
+                                         </p>
+                                        <?php }?>
+                                    </span></td>
+                                    <td style="width: 25%; padding-left: 0"><span style="font-size: 10px; ">
+                                        <?php if($result->observacoes != null){?>
+                                        <h2>Carro</h2>
+                                        <p>
+                                        <span style="font-size: 16px; "> <?php echo $result->observacoes?>
+                                        </p>
+                    <?php }?>
+                                    </span></td>
+                                </tr>
+                            </tbody>
+                        </table> 
+      
+                    </div>
+
+                    <div style="margin-top: 0; padding-top: 0">
+
+                    
+
+                    <?php if($result->defeito != null){?>
+                    <hr style="margin-top: 0">
+                    <h5>Defeito</h5>
+                    <p>
+                        <?php echo $result->defeito?>
+                    </p>
+                    <?php }?>
+                    <?php if($result->laudoTecnico != null){?>
+                    <hr style="margin-top: 0">
+                    <h5>Laudo Técnico</h5>
+                    <p>
+                        <?php echo $result->laudoTecnico?>
+                    </p>
+                    <?php }?>
+                    
+
+                        <?php if($produtos != null){?>
+                        <br />
+                        <table class="table table-bordered" id="tblProdutos">
+                                    <thead>
+                                        <tr>
+                                            <th>Produto</th>
+                                            <th>Sub-total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                       <?php
+                                        
+                                        foreach ($produtos as $p) {
+
+                                            $totalProdutos = $totalProdutos + $p->subTotal;
+                                            echo '<tr>';
+                                            echo '<td style="width: 75%; padding-left: 2"><span style="font-size: 12px; ">'.$p->descprod.'</span></td>';
+                                            
+                                            echo '<td><span style="font-size: 12px; ">R$ '.number_format($p->subTotal,2,',','.').'</span></td>';
+                                            echo '</tr>';
+                                        }?>
+
+                                        <tr>
+                                            <td colspan="1" style="text-align: right"><strong>Total:</strong></td>
+                                            <td><strong>R$ <?php echo number_format($totalProdutos,2,',','.');?></strong></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                               <?php }?>
+                        
+                        <?php if($servicos != null){?>
+                        <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Mão de Obra</th>
+                                                <th>Sub-total</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                        setlocale(LC_MONETARY, 'en_US');
+                                        foreach ($servicos as $s) {
+                                            $preco = $s->totalsrv;
+                                            $totalServico = $totalServico + $preco;
+                                            echo '<tr>';
+                                            echo '<td style="width: 75%; padding-left: 2"><span style="font-size: 12px; ">'.$s->descricao.'</span></td>';
+                                            echo '<td><span style="font-size: 12px; ">R$ '.number_format($s->totalsrv, 2, ',', '.').'</span></td>';
+                                            echo '</tr>';
+                                        }?>
+
+                                        <tr>
+                                            <td colspan="1" style="text-align: right"><strong>Total:</strong></td>
+                                            <td><strong>R$ <?php  echo number_format($totalServico, 2, ',', '.');?></strong></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                        <?php }?>
+                        <hr />
+                    
+                        <h4 style="text-align: right">Valor Total: R$ <?php echo number_format($totalProdutos + $totalServico,2,',','.');?></h4>
+
+                    </div>
+            
+
+                    
+                    
+              
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#imprimir").click(function(){         
+            PrintElem('#printOs');
+        })
+
+        function PrintElem(elem)
+        {
+            Popup($(elem).html());
+        }
+
+        function Popup(data)
+        {
+            var mywindow = window.open('', 'MapOs', 'height=600,width=800');
+            mywindow.document.write('<html><head><title>Map Os</title>');
+            mywindow.document.write("<link rel='stylesheet' href='<?php echo base_url();?>assets/css/bootstrap.min.css' />");
+            mywindow.document.write("<link rel='stylesheet' href='<?php echo base_url();?>assets/css/bootstrap-responsive.min.css' />");
+            mywindow.document.write("<link rel='stylesheet' href='<?php echo base_url();?>assets/css/matrix-style.css' />");
+            mywindow.document.write("<link rel='stylesheet' href='<?php echo base_url();?>assets/css/matrix-media.css' />");
+
+
+            mywindow.document.write("</head><body >");
+            mywindow.document.write(data);          
+            mywindow.document.write("</body></html>");
+
+            setTimeout(function(){
+                mywindow.print();
+            }, 50);
+
+            return true;
+        }
+
+    });
+</script>
