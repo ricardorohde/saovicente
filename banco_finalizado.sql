18ad02daceec8648470297e855d2b18cd868b766
Leandro <silveira.jedi@gmail.com>
2016-08-29 16:11:37 -0300

    Add files via upload

---
+++ banco_finalizado.sql
@@ -0,0 +1,406 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tempo de Geração: 27/07/2016 às 19:46
+-- Versão do servidor: 5.5.49-0ubuntu0.14.04.1
+-- Versão do PHP: 5.5.9-1ubuntu4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de dados: `saovicente`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `anexos`
+--
+
+CREATE TABLE IF NOT EXISTS `anexos` (
+  `idAnexos` int(11) NOT NULL AUTO_INCREMENT,
+  `anexo` varchar(45) DEFAULT NULL,
+  `thumb` varchar(45) DEFAULT NULL,
+  `url` varchar(300) DEFAULT NULL,
+  `path` varchar(300) DEFAULT NULL,
+  `os_id` int(11) NOT NULL,
+  PRIMARY KEY (`idAnexos`),
+  KEY `fk_anexos_os1` (`os_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Fazendo dump de dados para tabela `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('7684c3983d0a6fae371ca8fc2a5970b0', '::1', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36 Vivaldi/1.2.49', 1469659379, 'a:5:{s:9:"user_data";s:0:"";s:4:"nome";s:5:"admin";s:2:"id";s:1:"1";s:9:"permissao";s:1:"1";s:6:"logado";b:1;}');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `idClientes` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeCliente` varchar(255) NOT NULL,
+  `documento` varchar(20) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `celular` varchar(20) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `dataCadastro` date DEFAULT NULL,
+  `rua` varchar(70) DEFAULT NULL,
+  `numero` varchar(15) DEFAULT NULL,
+  `bairro` varchar(45) DEFAULT NULL,
+  `cidade` varchar(45) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `cep` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idClientes`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `documentos`
+--
+
+CREATE TABLE IF NOT EXISTS `documentos` (
+  `idDocumentos` int(11) NOT NULL AUTO_INCREMENT,
+  `documento` varchar(70) DEFAULT NULL,
+  `descricao` text,
+  `file` varchar(100) DEFAULT NULL,
+  `path` varchar(300) DEFAULT NULL,
+  `url` varchar(300) DEFAULT NULL,
+  `cadastro` date DEFAULT NULL,
+  `categoria` varchar(80) DEFAULT NULL,
+  `tipo` varchar(15) DEFAULT NULL,
+  `tamanho` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idDocumentos`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `emitente`
+--
+
+CREATE TABLE IF NOT EXISTS `emitente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `cnpj` varchar(45) DEFAULT NULL,
+  `ie` varchar(50) DEFAULT NULL,
+  `rua` varchar(70) DEFAULT NULL,
+  `numero` varchar(15) DEFAULT NULL,
+  `bairro` varchar(45) DEFAULT NULL,
+  `cidade` varchar(45) DEFAULT NULL,
+  `uf` varchar(20) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `url_logo` varchar(225) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Fazendo dump de dados para tabela `emitente`
+--
+
+INSERT INTO `emitente` (`id`, `nome`, `cnpj`, `ie`, `rua`, `numero`, `bairro`, `cidade`, `uf`, `telefone`, `email`, `url_logo`) VALUES
+(1, 'Mira Motos Oficina Especializada', '20.465.731/0001-10', '374.067.468.119', 'Rua Lindóia', '1012', 'Jardim Macucos', 'Itapira', 'SP', '(19) 3813-3228', 'le-mira@hotmail.com', 'http://localhost/miramotos/assets/uploads/8cbc979417c41394438aea82bce22076.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `itens_de_vendas`
+--
+
+CREATE TABLE IF NOT EXISTS `itens_de_vendas` (
+  `idItens` int(11) NOT NULL AUTO_INCREMENT,
+  `subTotal` varchar(45) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `vendas_id` int(11) NOT NULL,
+  `produtos_id` int(11) NOT NULL,
+  PRIMARY KEY (`idItens`),
+  KEY `fk_itens_de_vendas_vendas1` (`vendas_id`),
+  KEY `fk_itens_de_vendas_produtos1` (`produtos_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `lancamentos`
+--
+
+CREATE TABLE IF NOT EXISTS `lancamentos` (
+  `idLancamentos` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) DEFAULT NULL,
+  `valor` varchar(15) NOT NULL,
+  `data_vencimento` date NOT NULL,
+  `data_pagamento` date DEFAULT NULL,
+  `baixado` tinyint(1) DEFAULT NULL,
+  `cliente_fornecedor` varchar(255) DEFAULT NULL,
+  `forma_pgto` varchar(100) DEFAULT NULL,
+  `tipo` varchar(45) DEFAULT NULL,
+  `anexo` varchar(250) DEFAULT NULL,
+  `clientes_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idLancamentos`),
+  KEY `fk_lancamentos_clientes1` (`clientes_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `os`
+--
+
+CREATE TABLE IF NOT EXISTS `os` (
+  `idOs` int(11) NOT NULL AUTO_INCREMENT,
+  `dataInicial` date DEFAULT NULL,
+  `dataFinal` date DEFAULT NULL,
+  `garantia` varchar(45) DEFAULT NULL,
+  `descricaoProduto` varchar(150) DEFAULT NULL,
+  `defeito` varchar(150) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `observacoes` varchar(150) DEFAULT NULL,
+  `laudoTecnico` varchar(150) DEFAULT NULL,
+  `valorTotal` varchar(15) DEFAULT NULL,
+  `clientes_id` int(11) NOT NULL,
+  `usuarios_id` int(11) NOT NULL,
+  `lancamento` int(11) DEFAULT NULL,
+  `faturado` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idOs`),
+  KEY `fk_os_clientes1` (`clientes_id`),
+  KEY `fk_os_usuarios1` (`usuarios_id`),
+  KEY `fk_os_lancamentos1` (`lancamento`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `permissoes`
+--
+
+CREATE TABLE IF NOT EXISTS `permissoes` (
+  `idPermissao` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
+  `permissoes` text,
+  `situacao` tinyint(1) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`idPermissao`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Fazendo dump de dados para tabela `permissoes`
+--
+
+INSERT INTO `permissoes` (`idPermissao`, `nome`, `permissoes`, `situacao`, `data`) VALUES
+(1, 'Administrador', 'a:38:{s:8:"aCliente";s:1:"1";s:8:"eCliente";s:1:"1";s:8:"dCliente";s:1:"1";s:8:"vCliente";s:1:"1";s:8:"aProduto";s:1:"1";s:8:"eProduto";s:1:"1";s:8:"dProduto";s:1:"1";s:8:"vProduto";s:1:"1";s:8:"aServico";s:1:"1";s:8:"eServico";s:1:"1";s:8:"dServico";s:1:"1";s:8:"vServico";s:1:"1";s:3:"aOs";s:1:"1";s:3:"eOs";s:1:"1";s:3:"dOs";s:1:"1";s:3:"vOs";s:1:"1";s:6:"aVenda";s:1:"1";s:6:"eVenda";s:1:"1";s:6:"dVenda";s:1:"1";s:6:"vVenda";s:1:"1";s:8:"aArquivo";s:1:"1";s:8:"eArquivo";s:1:"1";s:8:"dArquivo";s:1:"1";s:8:"vArquivo";s:1:"1";s:11:"aLancamento";s:1:"1";s:11:"eLancamento";s:1:"1";s:11:"dLancamento";s:1:"1";s:11:"vLancamento";s:1:"1";s:8:"cUsuario";s:1:"1";s:9:"cEmitente";s:1:"1";s:10:"cPermissao";s:1:"1";s:7:"cBackup";s:1:"1";s:8:"rCliente";s:1:"1";s:8:"rProduto";s:1:"1";s:8:"rServico";s:1:"1";s:3:"rOs";s:1:"1";s:6:"rVenda";s:1:"1";s:11:"rFinanceiro";s:1:"1";}', 1, '2014-09-03'),
+(2, 'Usuarios', 'a:38:{s:8:"aCliente";s:1:"1";s:8:"eCliente";s:1:"1";s:8:"dCliente";s:1:"1";s:8:"vCliente";s:1:"1";s:8:"aProduto";s:1:"1";s:8:"eProduto";s:1:"1";s:8:"dProduto";s:1:"1";s:8:"vProduto";s:1:"1";s:8:"aServico";s:1:"1";s:8:"eServico";s:1:"1";s:8:"dServico";s:1:"1";s:8:"vServico";s:1:"1";s:3:"aOs";s:1:"1";s:3:"eOs";s:1:"1";s:3:"dOs";s:1:"1";s:3:"vOs";s:1:"1";s:6:"aVenda";s:1:"1";s:6:"eVenda";s:1:"1";s:6:"dVenda";s:1:"1";s:6:"vVenda";s:1:"1";s:8:"aArquivo";b:0;s:8:"eArquivo";b:0;s:8:"dArquivo";b:0;s:8:"vArquivo";b:0;s:11:"aLancamento";s:1:"1";s:11:"eLancamento";s:1:"1";s:11:"dLancamento";s:1:"1";s:11:"vLancamento";s:1:"1";s:8:"cUsuario";b:0;s:9:"cEmitente";b:0;s:10:"cPermissao";b:0;s:7:"cBackup";s:1:"1";s:8:"rCliente";s:1:"1";s:8:"rProduto";s:1:"1";s:8:"rServico";s:1:"1";s:3:"rOs";s:1:"1";s:6:"rVenda";s:1:"1";s:11:"rFinanceiro";s:1:"1";}', 1, '2016-04-06');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `idProdutos` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(80) NOT NULL,
+  `unidade` varchar(10) DEFAULT NULL,
+  `precoCompra` decimal(10,2) DEFAULT NULL,
+  `precoVenda` decimal(10,2) NOT NULL,
+  `estoque` int(11) NOT NULL,
+  `estoqueMinimo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idProdutos`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos_os`
+--
+
+CREATE TABLE IF NOT EXISTS `produtos_os` (
+  `idProdutos_os` int(11) NOT NULL AUTO_INCREMENT,
+  `quantidade` int(11) NOT NULL,
+  `os_id` int(11) NOT NULL,
+  `produtos_id` int(11) NOT NULL,
+  `subTotal` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`idProdutos_os`),
+  KEY `fk_produtos_os_os1` (`os_id`),
+  KEY `fk_produtos_os_produtos1` (`produtos_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servicos`
+--
+
+CREATE TABLE IF NOT EXISTS `servicos` (
+  `idServicos` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `descricao` varchar(45) DEFAULT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`idServicos`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servicos_os`
+--
+
+CREATE TABLE IF NOT EXISTS `servicos_os` (
+  `idServicos_os` int(11) NOT NULL AUTO_INCREMENT,
+  `os_id` int(11) NOT NULL,
+  `servicos_id` int(11) NOT NULL,
+  `subTotal` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`idServicos_os`),
+  KEY `fk_servicos_os_os1` (`os_id`),
+  KEY `fk_servicos_os_servicos1` (`servicos_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `idUsuarios` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
+  `rg` varchar(20) DEFAULT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `rua` varchar(70) DEFAULT NULL,
+  `numero` varchar(15) DEFAULT NULL,
+  `bairro` varchar(45) DEFAULT NULL,
+  `cidade` varchar(45) DEFAULT NULL,
+  `estado` varchar(20) DEFAULT NULL,
+  `email` varchar(80) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `celular` varchar(20) DEFAULT NULL,
+  `situacao` tinyint(1) NOT NULL,
+  `dataCadastro` date NOT NULL,
+  `nivel` int(11) NOT NULL,
+  `permissoes_id` int(11) NOT NULL,
+  PRIMARY KEY (`idUsuarios`),
+  KEY `fk_usuarios_permissoes1_idx` (`permissoes_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Fazendo dump de dados para tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuarios`, `nome`, `rg`, `cpf`, `rua`, `numero`, `bairro`, `cidade`, `estado`, `email`, `senha`, `telefone`, `celular`, `situacao`, `dataCadastro`, `nivel`, `permissoes_id`) VALUES
+(1, 'admin', '111', '111', '111', '111', '111', 'Itapira', 'SP', 'leandro.jsilveira@fatec.sp.gov.br', '0808a30d84b8858f1f900401161e0ecf674ac4c0', '0000-0000', '', 1, '2013-11-22', 1, 1),
+(3, 'Mira Motos', '111', '20.465.731/0001-10', 'Rua Lindóia', '1012', 'Jardim Macucos', 'Itapira', 'SP', 'le-mira@hotmail.com', '91aa067284835743ba67fd478ff98e8885d49dfa', '(19) 3813-3228', '', 1, '2016-04-06', 0, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendas`
+--
+
+CREATE TABLE IF NOT EXISTS `vendas` (
+  `idVendas` int(11) NOT NULL AUTO_INCREMENT,
+  `dataVenda` date DEFAULT NULL,
+  `valorTotal` varchar(45) DEFAULT NULL,
+  `desconto` varchar(45) DEFAULT NULL,
+  `faturado` tinyint(1) DEFAULT NULL,
+  `clientes_id` int(11) NOT NULL,
+  `usuarios_id` int(11) DEFAULT NULL,
+  `lancamentos_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idVendas`),
+  KEY `fk_vendas_clientes1` (`clientes_id`),
+  KEY `fk_vendas_usuarios1` (`usuarios_id`),
+  KEY `fk_vendas_lancamentos1` (`lancamentos_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Restrições para dumps de tabelas
+--
+
+--
+-- Restrições para tabelas `anexos`
+--
+ALTER TABLE `anexos`
+  ADD CONSTRAINT `fk_anexos_os1` FOREIGN KEY (`os_id`) REFERENCES `os` (`idOs`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `itens_de_vendas`
+--
+ALTER TABLE `itens_de_vendas`
+  ADD CONSTRAINT `fk_itens_de_vendas_vendas1` FOREIGN KEY (`vendas_id`) REFERENCES `vendas` (`idVendas`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_itens_de_vendas_produtos1` FOREIGN KEY (`produtos_id`) REFERENCES `produtos` (`idProdutos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `lancamentos`
+--
+ALTER TABLE `lancamentos`
+  ADD CONSTRAINT `fk_lancamentos_clientes1` FOREIGN KEY (`clientes_id`) REFERENCES `clientes` (`idClientes`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `os`
+--
+ALTER TABLE `os`
+  ADD CONSTRAINT `fk_os_clientes1` FOREIGN KEY (`clientes_id`) REFERENCES `clientes` (`idClientes`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_os_lancamentos1` FOREIGN KEY (`lancamento`) REFERENCES `lancamentos` (`idLancamentos`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_os_usuarios1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`idUsuarios`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `produtos_os`
+--
+ALTER TABLE `produtos_os`
+  ADD CONSTRAINT `fk_produtos_os_os1` FOREIGN KEY (`os_id`) REFERENCES `os` (`idOs`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_produtos_os_produtos1` FOREIGN KEY (`produtos_id`) REFERENCES `produtos` (`idProdutos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `servicos_os`
+--
+ALTER TABLE `servicos_os`
+  ADD CONSTRAINT `fk_servicos_os_os1` FOREIGN KEY (`os_id`) REFERENCES `os` (`idOs`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_servicos_os_servicos1` FOREIGN KEY (`servicos_id`) REFERENCES `servicos` (`idServicos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_usuarios_permissoes1` FOREIGN KEY (`permissoes_id`) REFERENCES `permissoes` (`idPermissao`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para tabelas `vendas`
+--
+ALTER TABLE `vendas`
+  ADD CONSTRAINT `fk_vendas_clientes1` FOREIGN KEY (`clientes_id`) REFERENCES `clientes` (`idClientes`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_vendas_usuarios1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`idUsuarios`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_vendas_lancamentos1` FOREIGN KEY (`lancamentos_id`) REFERENCES `lancamentos` (`idLancamentos`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
